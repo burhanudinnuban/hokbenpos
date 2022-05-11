743c6e5c174b48895d15d37261d26bea75c8c71b
burhanudinnuban <burhanudinnuban@gmail.com>
2022-05-11 12:59:10 +0700

    add eslint config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './i18n/i18n';
-import {useTranslation} from 'react-i18next';
 import MainNavigation from './navigation';
 
-const App = () => {
-  const {t, i18n} = useTranslation();
-
-  return <MainNavigation />;
+const App   = () => {
+  return     <MainNavigation />;
 };
 
 export default App;
--- src/templates/HomeTemplate/index.js
+++ src/templates/HomeTemplate/index.js
@@ -1,12 +1,19 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {ButtonTextAtom} from '../../components';
-import {colors} from '../../themes';
+import {View, Text} from "react-native";
+import React from "react";
+import {ButtonTextAtom} from "../../components";
+import {colors} from "../../themes";
+import {useTranslation} from "react-i18next";
 
 const HomeTemplate = () => {
+  const {t, i18n} = useTranslation();
+
   return (
     <View>
-      <ButtonTextAtom title={'Oke'} color={colors.primary} />
+      <ButtonTextAtom
+        title={t("id")}
+        onPress={() => i18n.changeLanguage("id")}
+        color={colors.gray}
+      />
     </View>
   );
 };
